79b62c0279a4c79767830c74a0539f98b9dca20a
Durgesh9871 <devisingh9871686098@gmail.com>
2023-03-02 21:43:38 +0530

    hack

---
+++ frontend/src/DisplayData.jsx
@@ -0,0 +1,12 @@
+import { Box } from '@chakra-ui/react'
+import React from 'react'
+
+const DisplayData = () => {
+  return (
+    <Box>
+        <h1>hello world</h1>
+    </Box>
+  )
+}
+
+export {DisplayData}
