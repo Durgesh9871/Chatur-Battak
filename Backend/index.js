7f085a206957b9d6c1f408ab9e6e204644be3b81
sumit6675 <sumitchimkar0@gmail.com>
2023-03-02 22:13:40 +0530

    adding backend structure

---
+++ Backend/index.js
@@ -0,0 +1,25 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const connection=require("./config/db")
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Game");
+});
+
+app.listen(process.env.port, async () => {
+  try {
+    await connection;
+    console.log("Coonected to the database");
+  } catch (err) {
+    console.log("err", err);
+  }
+  console.log(`Server is live at PORT : ${process.env.port}`);
+});
