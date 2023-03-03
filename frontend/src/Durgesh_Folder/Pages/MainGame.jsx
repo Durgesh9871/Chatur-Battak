26f21840b7779b561be019a4ab3b8a2470788ad0
Durgesh9871 <devisingh9871686098@gmail.com>
2023-03-03 10:20:05 +0530

    file

--- frontend/src/Durgesh_Folder/Pages/MainGame.jsx
+++ frontend/src/Durgesh_Folder/Pages/MainGame.jsx
@@ -2,6 +2,7 @@ import { Box, Img } from '@chakra-ui/react'
 import React, { useState } from 'react'
 import "./mainGame.css"
 import sky from "./Sky_cloud.png"
+import croco from "./Croco.png"
 
 
 
@@ -12,14 +13,26 @@ const MainGame = () => {
   return (
     <Box className='mainGameBox' overflow="hidden" >
         {/*  Water is here ------------- */}
-       <Box className='waterMain' display="flex" justifyContent="space-around" > 
+        <Box className='waterMain'>
+       <Box  display="flex" justifyContent="space-around" > 
          <Box className='sky'>
             <Img src={sky} alt="sky1" height="300px" />
          </Box>
          <Box className='sky2'>
             <Img src={sky} alt="sky2" />
          </Box>
+         
         </Box>
+
+         {/* Croco */}
+         <Box className='croco'>
+           <Img src={croco}  alt="cro"  /> 
+        </Box>
+
+        
+
+        </Box>
+       
     </Box>
   )
 }
