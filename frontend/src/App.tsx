f5a194114d693cdf32317b4c1e3e34598a787892
Durgesh9871 <devisingh9871686098@gmail.com>
2023-03-03 23:26:23 +0530

    file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,10 +3,15 @@ import React from "react";
 import "./App.css";
 import { Box } from "@chakra-ui/react";
 import AllRoutes from "./Components/AllRoutes";
+import { MainGame } from "./Durgesh_Folder/Pages/MainGame";
+
+
 function App() {
   return (
     <Box>
-      <AllRoutes />
+      {/* <AllRoutes /> */}
+
+      <MainGame />
     </Box>
   );
 }
