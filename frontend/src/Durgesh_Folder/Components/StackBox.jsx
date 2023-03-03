a89e9daf702074b67642fd2ea543d498104a07d7
Durgesh9871 <devisingh9871686098@gmail.com>
2023-03-03 23:32:03 +0530

    file

--- frontend/src/Durgesh_Folder/Components/StackBox.jsx
+++ frontend/src/Durgesh_Folder/Components/StackBox.jsx
@@ -5,8 +5,8 @@ const StackBox = ({count , color ,border,text}) => {
 
  
   return (
-    <Box border={`2px  ${border}`} height="30px" width="100px"  background={color} color={text} padding="10px">
-         <h1>{count}</h1>
+    <Box border={`2px  ${border}`} height="43px" textAlign="center" width="100px"  background={color} color={text} padding="10px">
+         <h1 style={{fontSize:"18px" , fontWeight:"700"}}>{count}</h1>
       
     </Box>
   )
