e1c49dd16e0cfa25e3395dee42008509c6744033
Durgesh9871 <devisingh9871686098@gmail.com>
2023-03-03 07:19:02 +0530

    file

--- frontend/src/Durgesh_Folder/Pages/MainGame.jsx
+++ frontend/src/Durgesh_Folder/Pages/MainGame.jsx
@@ -2,10 +2,12 @@ import { Box } from '@chakra-ui/react'
 import React from 'react'
 import "./mainGame.css"
 
+
+
 const MainGame = () => {
   return (
     <Box className='hello'>
-        <h1 id='hello' style={{fontSize:"222pxj"}} >hello</h1>
+        <h1 id='mainHeading' >hello</h1>
     </Box>
   )
 }
