281aa711f61b908157d8900c29751d73634f620d
Durgesh9871 <devisingh9871686098@gmail.com>
2023-03-03 07:47:30 +0530

    file

--- frontend/src/Durgesh_Folder/Pages/MainGame.jsx
+++ frontend/src/Durgesh_Folder/Pages/MainGame.jsx
@@ -6,20 +6,12 @@ import "./mainGame.css"
 
 const MainGame = () => {
     
-    const [flamethrowerActive, setFlamethrowerActive] = useState(false);
-
-  const activateFlamethrower = () => {
-    setFlamethrowerActive(true);
-    setTimeout(() => {
-      setFlamethrowerActive(false);
-    }, 3000); // stop animation after 3 seconds
-  };
+    
 
 
   return (
     <Box className='mainGameBox'>
-       <button onClick={activateFlamethrower}>Activate Flamethrower</button>
-      <div className={`flamethrower ${flamethrowerActive ? 'active' : ''}`}></div>
+     
     </Box>
   )
 }
